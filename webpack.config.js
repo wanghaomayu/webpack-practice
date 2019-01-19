d4ac36ae81ab49fdea6c0bcc1ce436ea77240e51
王浩 <wanghao.pivot@bytedance.com>
2019-01-19 18:23:47 +0800

    添加html-webpack-plugin和clean-webpack-plugin
    Change-Id: Ic893a3366c447f98decab559e7e55633ff72a9ec

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/print.js'
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: "管理输出."
+    })
+  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, 'dist')
