80afb44ec6da37ebd84e8e0140b27ff287915913
王浩 <wanghao.pivot@bytedance.com>
2019-01-19 16:09:11 +0800

    代码回退，为了下面的练习
    Change-Id: Ieaf5a66318ff6dbf28d5e4e1e98af7ccb0111e38

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,5 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|jpeg|svg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
   }
 };
