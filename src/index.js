7e6f4bd4de0422c1274e8c2554508f9a18c25af6
王浩 <wanghao.pivot@bytedance.com>
2019-01-24 12:16:06 +0800

    HMR 相关
    Change-Id: Ibefb5cc4bfe93930cbcfd15c40984c34ff61deaf

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 import _ from 'lodash';
 import printMe from './print';
+import './style.css';
 
 function component() {
   var element = document.createElement('div');
+  element.innerHTML = _.join(['啥','玩','意'], '-');
+
   var btn = document.createElement('button');
-
-  element.innerHTML = _.join(['Hello', 'Webpack'], '  ');
-  element.classList.add('hello');
-
-  btn.innerHTML = 'CLick me';
+  btn.innerHTML = "点我";
   btn.onclick = printMe;
 
   element.appendChild(btn);
+
   return element;
 }
-// ppp分支
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
 
 if (module.hot) {
   module.hot.accept('./print.js', function () {
-    console.log('Accepting the updated printMe module!');
-    printMe();
+    console.log('接受printMe函数的更新');
+
+    document.body.removeChild(element);
+    element = component();
+    document.body.appendChild(element);
   })
 }
